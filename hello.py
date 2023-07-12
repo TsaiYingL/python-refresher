d4f28f0a1b60b0061704a20658eefad6cb87c11b
Tsai <tsaiying11100815@gmail.com>
2023-07-11 20:10:34 -0400

    need to fix requirments.txt

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def hello():
     return "Hello, world!"
 
--- test_hello.py
+++ test_hello.py
@@ -11,22 +11,22 @@ class TestHello(unittest.TestCase):
     def test_sin(self):
         self.assertEqual(hello.sin(0), 0)
         self.assertEqual(hello.sin(1), 0.8414709848078965)
-        self.assertEqual(hello.sin(np.pi/2),1)
+        self.assertEqual(hello.sin(np.pi / 2), 1)
 
     def test_cos(self):
         self.assertEqual(hello.cos(0), 1)
         self.assertEqual(hello.cos(1), 0.5403023058681398)
-        self.assertEqual(hello.cos(np.pi),-1)
+        self.assertEqual(hello.cos(np.pi), -1)
 
     def test_tan(self):
         self.assertEqual(hello.tan(0), 0)
         self.assertEqual(hello.tan(1), 1.5574077246549023)
-        self.assertEqual(round(hello.tan(np.pi)),0)
+        self.assertEqual(round(hello.tan(np.pi)), 0)
 
     def test_cot(self):
         self.assertEqual(hello.cot(0), float("inf"))
         self.assertEqual(hello.cot(1), 0.6420926159343306)
-        self.assertEqual(round(hello.cot(np.pi/2)), 0)
+        self.assertEqual(round(hello.cot(np.pi / 2)), 0)
 
 
 if __name__ == "__main__":
