d31f65e2378fb12bb7010ed47e5181098ae8d62a
tsai <tsaying11100815@gmail.com>
2023-07-13 13:52:42 -0400

    python: fix suggestions mentioned in comments

--- physics.py
+++ physics.py
@@ -2,34 +2,45 @@
 in water. It also tells us if the object will float on water"""
 
 # adding values
-v = 0
-mass = 0
-depth = int
 density_fluid = 1000
 g = 9.81
 
 
 # calculate and return the buoyancy
 def calculate_buoyancy(v):
-    return f"the buoyancy is {v*density_fluid*g} Newtons"
+    if v >= 0 and density_fluid >= 0:
+        buoyancy = v * density_fluid * g
+        return buoyancy
+    else:
+        return "error"
 
 
 # tell the user if the object will flaot or not
 def will_it_float(v, mass):
-    return v * density_fluid * g > mass * g
+    if v >= 0 and density_fluid >= 0 and mass >= 0:
+        will_float = v * density_fluid * g > mass * g
+        return will_float
+    else:
+        return "error"
 
 
 # calculate and return the pressure
 def calculate_pressure(depth):
-    surface_pressure = 101324
-    return f"the pressure is {density_fluid*depth*g+surface_pressure} Pascals"
+    if density_fluid >= 0:
+        if depth < 0:
+            depth = abs(depth)
+        surface_pressure = 101324
+        pressure = density_fluid * depth * g + surface_pressure
+        return pressure
+    else:
+        return "error"
 
 
-# next time can return th =e value instead of a string
-# don't need to make a class sine the question specified to make a function
-# we use classes when we have multiple objects
+# √√ next time can return the value instead of a string
+# √√ don't need to make a class sine the question specified to make a function
+# √√ we use classes when we have multiple objects
 # the volume, depth, and density cannot be less than 0
-# command d can select the same thing that you initially selected
-# might not need to set v, mass, and depth to 0 st the start
-# elif: only use it when the cases are related to each other
-# when returning the value, define the equation so we know what it is when debugging
+# √√ command d can select the same thing that you initially selected
+# √√ might not need to set v, mass, and depth to 0 st the start
+# √√ elif: only use it when the cases are related to each other
+# √√ when returning the value, define the equation so we know what it is when debugging
--- test_physics.py
+++ test_physics.py
@@ -4,26 +4,17 @@ import physics
 
 class TestPhysics(unittest.TestCase):
     def test_buoyancy(self):
-        self.assertEqual(
-            physics.calculate_buoyancy(10),
-            f"the buoyancy is {10*1000*9.81} Newtons",
-        )
-        self.assertNotEqual(
-            physics.calculate_buoyancy(30), f"the buoyancy is {10*1000*9.81} Newtons"
-        )
+        self.assertEqual(physics.calculate_buoyancy(10), 10 * 1000 * 9.81)
+        self.assertNotEqual(physics.calculate_buoyancy(30), 10 * 1000 * 9.81)
 
     def test_will_it_float(self):
         self.assertEqual(physics.will_it_float(100, 1), True)
         self.assertEqual(physics.will_it_float(1, 1001), False)
-        self.assertNotEqual(physics.will_it_float(1, 1000), False)
+        self.assertNotEqual(physics.will_it_float(100, 1), False)
 
     def test_calculate_pressure(self):
-        self.assertEqual(
-            physics.calculate_pressure(100), f"the pressure is {1000*100*9.81} Pascals"
-        )
-        self.assertNotEqual(
-            physics.calculate_buoyancy(100), f"the pressure is {1000*90*9.81} Pascals"
-        )
+        self.assertEqual(physics.calculate_pressure(100), 1000 * 100 * 9.81 + 101324)
+        self.assertNotEqual(physics.calculate_buoyancy(100), 1000 * 90 * 9.81 + 101324)
 
 
 if __name__ == "__main__":
