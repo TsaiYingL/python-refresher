542067171f6694b891c426ba853992b63388fd6f
tsai <tsaying11100815@gmail.com>
2023-07-13 08:28:24 -0400

    added test for bank

--- test_bank.py
+++ test_bank.py
@@ -1,2 +1,18 @@
 import unittest
 import bank
+
+
+class Test_bank(unittest.TestCase):
+    def test_account(self):
+        acc = bank.Bankaccount("Tsai", 1234567, 0)
+        bank.Bankaccount.current_balance(acc)
+        self.assertNotEqual(
+            bank.Bankaccount.deposit(acc, 100), "Tsai has $200 in account."
+        )
+        self.assertEqual(
+            bank.Bankaccount.deposit(acc, 100), "Tsai has $200 in account."
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
